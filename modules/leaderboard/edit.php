44f0139e13c64278555afcc10444304fe1f74043
Josh Sherman <josh@gravityblvd.com>
2013-12-17 14:07:45 -0500

    Cleaning up some stuff, added some rewrites

---
+++ modules/leaderboard/edit.php
@@ -0,0 +1,15 @@
+<?php
+
+class leaderboard_edit extends leaderboard_new
+{
+	public function __default()
+	{
+		// Looks up the leaderboard by UID
+
+		// Checks that the leaderboard is owned by this user
+
+		// Returns the leaderboard
+	}
+}
+
+?>
