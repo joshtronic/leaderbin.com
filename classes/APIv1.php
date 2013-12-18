7b18541a61175e9f18ec8468e48a7ddb2479b9ed
Josh Sherman <josh@gravityblvd.com>
2013-12-17 23:34:39 -0500

    Working on that API scaffolding

---
+++ classes/APIv1.php
@@ -0,0 +1,15 @@
+<?php
+
+class APIv1 extends Module
+{
+	protected $request_method = 'GET';
+
+	public function __construct()
+	{
+		// Checks the request method
+
+		// Checks the key
+	}
+}
+
+?>
---
+++ modules/api/v1/leaderboards.php
@@ -0,0 +1,11 @@
+<?php
+
+class api_v1_leaderboards extends APIv1
+{
+	public function __default()
+	{
+		
+	}
+}
+
+?>
